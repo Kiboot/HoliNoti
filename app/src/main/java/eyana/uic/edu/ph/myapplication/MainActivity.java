171066f3f27477469009076c508a4b43bf09787a
Kiboot <kiboot@users.noreply.github.com>
2019-05-29 02:09:24 +0800

    created second activity

--- app/src/main/java/eyana/uic/edu/ph/myapplication/MainActivity.java
+++ app/src/main/java/eyana/uic/edu/ph/myapplication/MainActivity.java
@@ -1,13 +1,38 @@
 package eyana.uic.edu.ph.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.Date;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.add_btn);
+
+        int day =   Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+
+
+        button.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View arg0) {
+
+        // Start NewActivity.class
+        Intent myIntent = new Intent(MainActivity.this, createholiday.class);
+        //Intent myIntent = new Intent("eyana.uic.edu.ph.myapplication.createholiday");
+        startActivity(myIntent);
     }
 }
---
+++ app/src/main/java/eyana/uic/edu/ph/myapplication/createholiday.java
@@ -0,0 +1,15 @@
+package eyana.uic.edu.ph.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class createholiday extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.createholiday);
+
+
+    }
+}
